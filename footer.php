a27cf5d289f62e805bcc22e5ca65010c8a0a27fc
ensignrolaren <kelseybarmettler@gmail.com>
2022-09-05 09:03:15 -0700

    enable footer block area

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -12,25 +13,12 @@
 ?>
 
 	<div class="site-footer-wrapper">
-		<footer id="colophon" class="site-footer">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rad' ) ); ?>">
-					<?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'rad' ), 'WordPress' );
-					?>
-				</a>
-				<span class="sep"> | </span>
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'rad' ), 'rad', '<a href="http://radicaladvantagedigital.com">Radical Advantage Digital</a>' );
-					?>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
+		<?php dynamic_sidebar('footer-blocks-area'); ?>
 	</div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
