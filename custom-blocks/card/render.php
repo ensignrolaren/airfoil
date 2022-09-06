892923e6297967016b780a3101592d8a85d30bd5
ensignrolaren <kelseybarmettler@gmail.com>
2022-09-05 19:55:55 -0700

    improvements to card block

--- custom-blocks/card/render.php
+++ custom-blocks/card/render.php
@@ -1,9 +1,29 @@
-<div class="">
+<?php
+
+/**
+ * Card block rendering
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create class attribute allowing for custom "className" and "align" values.
+$classes = '';
+if (!empty($block['className'])) {
+	$classes .= sprintf(' %s', $block['className']);
+}
+if (!empty($block['align'])) {
+	$classes .= sprintf(' align%s', $block['align']);
+}
+?>
+<div class="rad-card <?php echo esc_attr($classes); ?>">
 	<?php
 	$image = get_field('image');
 	?>
 	<!-- Card image -->
-	<div class="card__image">
+	<div class="rad-card__image">
 		<?php if ($image) : ?>
 			<?php echo wp_get_attachment_image($image['id'], 'full'); ?>
 		<?php endif; ?>
@@ -20,7 +40,7 @@
 	);
 	?>
 	<!-- Card text -->
-	<div class="card__body">
+	<div class="rad-card__body">
 		<InnerBlocks template=" . esc_attr(wp_json_encode($template)) . " />
 	</div>
 </div>
