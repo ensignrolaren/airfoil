f139be8b58c8ec34211cfd7adff3920cb7ef7ecc
ensignrolaren <kelseybarmettler@gmail.com>
2022-10-11 20:22:09 -0700

    supporting text alignment for copyright block

--- custom-blocks/copyright/render.php
+++ custom-blocks/copyright/render.php
@@ -9,8 +9,34 @@
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
 ?>
-<div class="rad-copyright">
 <?php
-	echo '&copy; ' . date("Y");
-	echo ' ';
-	echo bloginfo('name');
+// Create class attribute allowing for custom "className" and "align" values.
+$classes = ['rad-copyright'];
+if (!empty($block['className'])) {
+	$classes = array_merge($classes, explode(' ', $block['className']));
+}
+if (!empty($block['align'])) {
+	$classes[] = 'align' . $block['align'];
+}
+if (!empty($block['align_text'])) {
+	$classes[] = 'has-text-align-' . $block['align_text'];
+}
+
+if (!empty($block['backgroundColor'])) {
+	$classes[] = 'has-background';
+	$classes[] = 'has-' . $block['backgroundColor'] . '-background-color';
+}
+if (!empty($block['textColor'])) {
+	$classes[] = 'has-text-color';
+	$classes[] = 'has-' . $block['textColor'] . '-color';
+}
+printf(
+	'<div class="%s"%s>',
+	esc_attr(join(' ', $classes)),
+	!empty($block['anchor']) ? ' id="' . esc_attr(sanitize_title($block['anchor'])) . '"' : '',
+); ?>
+<?php
+echo '&copy; ' . date("Y");
+echo ' ';
+echo bloginfo('name'); ?>
+</div>
