e86ec9a0ceff047512dea479efa8bf9c5f48f069
ensignrolaren <kelseybarmettler@gmail.com>
2022-11-03 18:03:39 -0700

    Load navigation.js if ACF isn't activated

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -11,6 +11,8 @@ function rad_frontend_assets() {
 
 	// wp_dequeue_style('wp-block-library');
 	// wp_deregister_style('wp-block-library');
+
+	// Only enqueue some extra scripts if the options page says we want them
 	if (class_exists('ACF')) {
 		if (get_field('responsive_menu', 'option') == 'responsive-menu') {
 			wp_enqueue_script('rad-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
@@ -21,6 +23,11 @@ function rad_frontend_assets() {
 		if (is_singular() && comments_open() && get_option('thread_comments')) {
 			wp_enqueue_script('comment-reply');
 		}
+	} else {
+		// when in doubt, load ALL THE THINGS
+		wp_enqueue_script('rad-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+		wp_enqueue_script('rad-sticky-nav', get_template_directory_uri() . '/js/sticky.js', array(), _S_VERSION, true);
+		wp_enqueue_script('comment-reply');
 	}
 }
 add_action('wp_enqueue_scripts', 'rad_frontend_assets', 100);
